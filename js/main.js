9f008092440512742a8c6b54b4b342a8ebe23ec2
robs-am <roberta.amaro89@gmail.com>
2022-06-23 21:20:36 -0300

    add validacao formulario js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-
 function login() {
-    window.location.href= "login.html";
-    
+    window.location.href = "login.html";
+
 }
 
 function signup() {
-    window.location.href="signup.html"
+    window.location.href = "signup.html"
+}
+
+function validateFields() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        document.getElementById('fPassword').disabled = true;
+    } else { (validateEmail(email)) {
+        document.getElementById('fPassword').disabled = false;
+    }
+
+    }
+
+    function ValidateEmail(email) {
+        return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email));
+        
+    }
+
+    //pegar o valorr do campo do 
+    //verificar se o e-mail é validoemail
+    //v, habilitar recuperar senha
+    //f, desabilitar recuperar semha
+
 }
