4f5e3f2a2ee05c2779d759f23d848ad38698099d
robs-am <roberta.amaro89@gmail.com>
2022-06-23 22:28:35 -0300

    add firebase

--- js/main.js
+++ js/main.js
@@ -7,24 +7,12 @@ function signup() {
     window.location.href = "signup.html"
 }
 
-function validateFields() {
-    const email = document.getElementById("email").value;
-    if (!email) {
-        document.getElementById('fPassword').disabled = true;
-    } else { (validateEmail(email)) {
-        document.getElementById('fPassword').disabled = false;
-    }
-
-    }
-
-    function ValidateEmail(email) {
-        return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email));
-        
-    }
-
-    //pegar o valorr do campo do 
-    //verificar se o e-mail é validoemail
-    //v, habilitar recuperar senha
-    //f, desabilitar recuperar semha
-
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyCxHjnIj8BocLL1DBJEWF8XuTkullOaKxI",
+    authDomain: "login-js-10055.firebaseapp.com",
+    projectId: "login-js-10055",
+    storageBucket: "login-js-10055.appspot.com",
+    messagingSenderId: "542666832249",
+    appId: "1:542666832249:web:cd212e4727303ba3a2d7a8",
+    measurementId: "G-PF106Y6XRE"
+  };
