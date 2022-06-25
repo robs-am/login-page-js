dd8baf91c4a71d3110eeffc1223368b83325221b
robs-am <roberta.amaro89@gmail.com>
2022-06-24 23:29:52 -0300

    add usuario autentificado

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 function login() {
+    console.log('antes');
+    firebase.auth().signInWithEmailAndPassword("roberta.amaro89@gmail.com", "123456").then(response => {
+        console.log('success', response)
+    }).catch(error => {
+        console.log('error', error)
+    });
+    console.log('depois')
     window.location.href = "login.html";
+
 
 }
 
 function signup() {
     window.location.href = "signup.html"
 }
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyCxHjnIj8BocLL1DBJEWF8XuTkullOaKxI",
@@ -15,4 +24,13 @@ const firebaseConfig = {
     messagingSenderId: "542666832249",
     appId: "1:542666832249:web:cd212e4727303ba3a2d7a8",
     measurementId: "G-PF106Y6XRE"
-  };
+};
+firebase.initializeApp(firebaseConfig);
+
+console.log('antes');
+firebase.auth().signInWithEmailAndPassword("roberta.amaro89@gmail.com", "123456").then(response => {
+    console.log('success', response)
+}).catch(error => {
+    console.log('error', error)
+});
+console.log('depois')
