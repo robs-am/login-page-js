03c91802a65e85ee20207b1f38368faaeac9ac81
robs-am <roberta.amaro89@gmail.com>
2022-06-25 15:04:05 -0300

    fix js login signup errors

--- js/main.js
+++ js/main.js
@@ -1,41 +1,14 @@
 
 function login() {
     
-    firebase.auth().signInWithEmailAndPassword(
-        form.email().value, form.password().value
-    ).then(response => {
-        window.location.href = "home.html";
-    }).catch(error => {
-        alert(getErrorMessage(error));
-    });
+     
+window.location.href = "login.html";
+
 }
-
-    
-    /* window.location.href = "login.html"; */
-
-
 
 
 function signup() {
-    window.location.href = "signup.html"
+    window.location.href = "signup.html";
 }
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCxHjnIj8BocLL1DBJEWF8XuTkullOaKxI",
-    authDomain: "login-js-10055.firebaseapp.com",
-    projectId: "login-js-10055",
-    storageBucket: "login-js-10055.appspot.com",
-    messagingSenderId: "542666832249",
-    appId: "1:542666832249:web:cd212e4727303ba3a2d7a8",
-    measurementId: "G-PF106Y6XRE"
-};
-firebase.initializeApp(firebaseConfig);
-
-console.log('antes');
-firebase.auth().signInWithEmailAndPassword("roberta.amaro89@gmail.com", "123456").then(response => {
-    window.location.href = "login.html"
-}).catch(error => {
-    console.log('error', error)
-});
-console.log('depois')
